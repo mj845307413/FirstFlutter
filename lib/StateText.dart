34026ff1dae0644e3707e02156c9b4bac1ce43cc
chocolatema <chocolatema@qiyi.com>
2020-07-07 19:40:53 +0800

    ListView

--- lib/StateText.dart
+++ lib/StateText.dart
@@ -1,5 +1,6 @@
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
+import 'package:flutterapp/widget/MyDrawer.dart';
 
 import 'RandomWordsWidget.dart';
 
@@ -17,26 +18,43 @@ class StateText extends State<RandomWordsWidget> {
   final _saved = new Set<WordPair>();
 
   Widget _buildSuggestions() {
-    return new ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        // 对于每个建议的单词对都会调用一次itemBuilder，然后将单词对添加到ListTile行中
-        // 在偶数行，该函数会为单词对添加一个ListTile row.
-        // 在奇数行，该函数会添加一个分割线widget，来分隔相邻的词对。
-        // 注意，在小屏幕上，分割线看起来可能比较吃力。
-        itemBuilder: (context, i) {
-          // 在每一列之前，添加一个1像素高的分隔线widget
-          if (i.isOdd) return new Divider();
+    return Column(
+      children: <Widget>[
+        ListTile(title:Text("商品列表")),
+        Expanded(
+          child: new ListView.separated(
+            itemCount: 61,
+            physics: BouncingScrollPhysics(),
+            padding: const EdgeInsets.all(16.0),
+            // 对于每个建议的单词对都会调用一次itemBuilder，然后将单词对添加到ListTile行中
+            // 在偶数行，该函数会为单词对添加一个ListTile row.
+            // 在奇数行，该函数会添加一个分割线widget，来分隔相邻的词对。
+            // 注意，在小屏幕上，分割线看起来可能比较吃力。
 
-          // 语法 "i ~/ 2" 表示i除以2，但返回值是整形（向下取整），比如i为：1, 2, 3, 4, 5
-          // 时，结果为0, 1, 1, 2, 2， 这可以计算出ListView中减去分隔线后的实际单词对数量
-          final index = i ~/ 2;
-          // 如果是建议列表中最后一个单词对
-          if (index >= _suggestions.length) {
-            // ...接着再生成10个单词对，然后添加到建议列表
-            _suggestions.addAll(generateWordPairs().take(10));
-          }
-          return _buildRow(_suggestions[index]);
-        });
+            itemBuilder: (context, i) {
+              // 语法 "i ~/ 2" 表示i除以2，但返回值是整形（向下取整），比如i为：1, 2, 3, 4, 5
+              // 时，结果为0, 1, 1, 2, 2， 这可以计算出ListView中减去分隔线后的实际单词对数量
+              // 如果是建议列表中最后一个单词对
+              if (i >= _suggestions.length && _suggestions.length <= 50) {
+                // ...接着再生成10个单词对，然后添加到建议列表
+                _suggestions.addAll(generateWordPairs().take(10));
+              }
+              if (i >= _suggestions.length) {
+                return Container(
+                    alignment: Alignment.center,
+                    padding: EdgeInsets.all(16.0),
+                    child: Text(
+                      "没有更多了",
+                      style: TextStyle(color: Colors.grey),
+                    ));
+              }
+              return _buildRow(_suggestions[i]);
+            },
+            separatorBuilder: (context, index) => Divider(height: .0),
+          ),
+        ),
+      ],
+    );
   }
 
   Widget _buildRow(WordPair pair) {
@@ -67,18 +85,33 @@ class StateText extends State<RandomWordsWidget> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(
-        title: new Text('Startup Name Generator'),
-        actions: <Widget>[
-          new IconButton(
-            icon: new Icon(Icons.list),
-            onPressed: _pushSaved,
+        appBar: new AppBar(
+          leading: Icon(Icons.add_to_photos),
+          title: new Text('Startup Name Generator'),
+          actions: <Widget>[
+            new IconButton(
+              icon: new Icon(Icons.list),
+              onPressed: _pushSaved,
+            ),
+          ],
+        ),
+        endDrawer: MyDrawer(),
+        drawer: MyDrawer(),
+        body: _buildSuggestions(),
+        floatingActionButton: FloatingActionButton(onPressed: _pressAction),
+        floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+        bottomNavigationBar: BottomAppBar(
+          color: Colors.white,
+          shape: CircularNotchedRectangle(), // 底部导航栏打一个圆形的洞
+          child: Row(
+            children: [
+              IconButton(icon: Icon(Icons.home)),
+              SizedBox(), //中间位置空出
+              IconButton(icon: Icon(Icons.business)),
+            ],
+            mainAxisAlignment: MainAxisAlignment.spaceAround, //均分底部导航栏横向空间
           ),
-        ],
-      ),
-      body: _buildSuggestions(),
-      floatingActionButton: FloatingActionButton(onPressed: _pressAction),
-    );
+        ));
   }
 
   void _pressAction() {
